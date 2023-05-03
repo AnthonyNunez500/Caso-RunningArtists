8238ac8074d2d98d44869ea78b518fcd5ecd2259
Luis D. Maco <luisdmaco@outlook.com>
2023-05-02 21:49:56 -0500

    offers added + moustache fixed

--- crud-RunningArtists/src/app/app.module.ts
+++ crud-RunningArtists/src/app/app.module.ts
@@ -6,26 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './components/home/home.component';
-
+import { CardComponent } from './components/card/card.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToolbarComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, ToolbarComponent, HomeComponent, CardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
